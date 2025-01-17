ab00972ad4de57531e4c458d38daab4fdda1bf08
farmlink9633 <farmlink9633@gmail.com>
2025-01-17 17:01:57 +0530

    picture

--- lib/authscreens/login.dart
+++ lib/authscreens/login.dart
@@ -10,142 +10,184 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _terms = false; // Initialize the checkbox state
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back_ios),
-        backgroundColor: Colors.white,
-        foregroundColor: const Color.fromARGB(255, 4, 56, 4),
-      ),
-      body: Column(children: [
-        Container(width: 1800,height: 300,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  fit: BoxFit.fitHeight,
-                  image: NetworkImage("https://images.pexels.com/photos/1353938/pexels-photo-1353938.jpeg")),
-
-                  ),
-        ),
-      
-      Padding(
-        padding: const EdgeInsets.all(20.0),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        "https://images.pexels.com/photos/1353938/pexels-photo-1353938.jpeg",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
             Text(
               "Welcome back",
               style: GoogleFonts.josefinSans(
-                  fontSize: 35,
-                  color: const Color.fromARGB(255, 7, 75, 7),
-                  fontWeight: FontWeight.bold,
-                  ), 
+                fontSize: 35,
+                color: const Color.fromARGB(255, 7, 75, 7),
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            SizedBox(
-               height: 16
-            ),
+            SizedBox(height: 16),
             Text(
               "Login to your account",
               style: TextStyle(
-                  fontSize: 16,
-                  color: const Color.fromARGB(255, 198, 180, 180)),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-                fillColor: Color(0xFFC1DFCB),
-                filled: true,
-                label: Text("Email",
-                    style: TextStyle(
-                        fontSize: 15,
-                        color: const Color.fromARGB(255, 4, 56, 4),
-                        fontWeight: FontWeight.bold)),
+                fontSize: 16,
+                color: const Color.fromARGB(255, 198, 180, 180),
               ),
             ),
-            SizedBox(
-              height: 15,
-            ),
-
-            TextField(
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
                 decoration: InputDecoration(
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none),
-              fillColor: Color(0xFFC1DFCB),
-              filled: true,
-              label: Text("Password",
-                  style: TextStyle(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  fillColor: Color(0xFFC1DFCB),
+                  filled: true,
+                  label: Text(
+                    "Email",
+                    style: TextStyle(
                       fontSize: 15,
                       color: const Color.fromARGB(255, 4, 56, 4),
-                      fontWeight: FontWeight.bold)),
-            )),
-            SizedBox(
-              height: 4,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Checkbox(value: false, onChanged: (value) {
-                  
-                },),
-              Text("Remember Me",style: TextStyle(
-              fontSize: 11,color: const Color.fromARGB(255, 4, 46, 4) ,fontWeight: FontWeight.bold
-              )),
-              SizedBox(
-                width: 90 
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  fillColor: Color(0xFFC1DFCB),
+                  filled: true,
+                  label: Text(
+                    "Password",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: const Color.fromARGB(255, 4, 56, 4),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
-              
-              Text("Forgot Password",style: TextStyle(
-                fontSize: 12,color: const Color.fromARGB(255, 4, 46, 4) ,fontWeight: FontWeight.bold
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(activeColor: Colors.green,
+                        value: _terms,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _terms = value ?? false;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Remember Me",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: const Color.fromARGB(255, 4, 46, 4),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Add your forgot password logic here
+                    },
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: const Color.fromARGB(255, 4, 46, 4),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              ),
-              SizedBox(
-                height: 120 ,
-              ),
-        
-          ],),
-              ElevatedButton(
-                onPressed: () {},
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // Add your login logic here
+              },
               style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 4, 46, 4),
-                  fixedSize: Size(500, 11)),
+                backgroundColor: const Color.fromARGB(255, 4, 46, 4),
+                fixedSize: Size(300, 50),
+              ),
               child: Text(
                 "Login",
                 style: TextStyle(fontSize: 16, color: Colors.white),
               ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Row(
+            ),
+            SizedBox(height: 15),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              Text("Don't have an account?",style: TextStyle(
-                fontSize: 12,color: Colors.black,fontWeight: FontWeight.normal
-              )),
-              SizedBox(
-                width: 6,
-              ),
-            GestureDetector (
-              onTap:() {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> Registration()));
-              },
-                child: Text("Sign up",style: TextStyle(decoration:TextDecoration.underline,fontSize: 14,color: Color.fromARGB(255, 4, 46, 4),fontWeight: FontWeight.bold
+                Text(
+                  "Don't have an account?",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    fontWeight: FontWeight.normal,
+                  ),
                 ),
+                SizedBox(width: 6),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Registration(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 4, 46, 4),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-            ],)
-          
-        ]),
-    ),
-    ],
-    ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
